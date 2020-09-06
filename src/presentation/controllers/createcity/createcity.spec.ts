036e529dfeef8d70db1b3e11bea60dd068a1669e
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-06 20:41:23 -0300

    test: ensure CreateCityController returns 400 if no state is provided

--- src/presentation/controllers/createcity/createcity.spec.ts
+++ src/presentation/controllers/createcity/createcity.spec.ts
@@ -11,4 +11,15 @@ describe('Create City Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+  test('Should return 400 if no state is provided ', () => {
+    const sut = new CreateCityController()
+    const httpRequest = {
+      body: {
+        name: 'valid_name'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: state'))
+  })
 })
