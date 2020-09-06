f4ebf427138ff949d6f6c33bfb01a550edc3925d
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-06 20:52:54 -0300

    refactor: make CreateCityController conforms to a protocol

--- src/presentation/controllers/createcity/createcity.ts
+++ src/presentation/controllers/createcity/createcity.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { badRequest } from '../../helpers/http-helper'
+import { Controller } from '../../protocols/controller'
 
-export class CreateCityController {
+export class CreateCityController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name','state']
     for (const field of requiredFields) {
