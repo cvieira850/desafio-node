71eb6d0d39a3e9708526a8580c451be3343f7d3c
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-06 20:48:21 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/createcity/createcity.ts
+++ src/presentation/controllers/createcity/createcity.ts
@@ -1,19 +1,14 @@
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { MissingParamError } from '../../errors/missing-param-error'
+import { badRequest } from '../../helpers/http-helper'
 
 export class CreateCityController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name')
-      }
+      return badRequest(new MissingParamError('name'))
     }
     if (!httpRequest.body.state) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('state')
-      }
+      return badRequest(new MissingParamError('state'))
     }
   }
 }
