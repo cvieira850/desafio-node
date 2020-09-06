8bc3cb623a7bceb3952c708eff32abdca7ac62fd
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-06 20:50:34 -0300

    refactor: simplify code

--- src/presentation/controllers/createcity/createcity.ts
+++ src/presentation/controllers/createcity/createcity.ts
@@ -4,11 +4,11 @@ import { badRequest } from '../../helpers/http-helper'
 
 export class CreateCityController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.state) {
-      return badRequest(new MissingParamError('state'))
+    const requiredFields = ['name','state']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
