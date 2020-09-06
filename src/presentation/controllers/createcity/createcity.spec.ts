9311e71effe2eb04dcc5756671b4f5ff26e0d508
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-06 20:54:53 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/createcity/createcity.spec.ts
+++ src/presentation/controllers/createcity/createcity.spec.ts
@@ -1,9 +1,13 @@
 import { CreateCityController } from './createcity'
 import { MissingParamError } from '../../errors/missing-param-error'
 
+const makeSut = (): CreateCityController => {
+  return new CreateCityController()
+}
+
 describe('Create City Controller', () => {
   test('Should return 400 if no name is provided ', () => {
-    const sut = new CreateCityController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         state: 'valid_state'
@@ -14,7 +18,7 @@ describe('Create City Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
   test('Should return 400 if no state is provided ', () => {
-    const sut = new CreateCityController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'valid_name'
