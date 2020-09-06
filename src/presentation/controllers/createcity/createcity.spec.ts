151f186fac1fde9e520193c57488929e7843651b
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-06 20:34:17 -0300

    test: ensure CreateCityController returns 400 if no name is provided

---
+++ src/presentation/controllers/createcity/createcity.spec.ts
@@ -0,0 +1,13 @@
+import { CreateCityController } from './createcity'
+describe('Create City Controller', () => {
+  test('Should return 400 if no name is provided ', () => {
+    const sut = new CreateCityController()
+    const httpRequest = {
+      body: {
+        state: 'valid_state'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
