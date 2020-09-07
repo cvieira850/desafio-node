224ecaa1f5303907dbd2f1dd2a693f83473d3e15
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-06 21:08:51 -0300

    test: ensure CreateCityController returns 400 if an invalid state is provided

--- src/presentation/controllers/createcity/createcity.spec.ts
+++ src/presentation/controllers/createcity/createcity.spec.ts
@@ -1,13 +1,30 @@
 import { CreateCityController } from './createcity'
 import { MissingParamError } from '../../errors/missing-param-error'
+import { InvalidParamError } from '../../errors/invalid-param-error'
+import { StateValidator } from '../../protocols/state-validator'
 
-const makeSut = (): CreateCityController => {
-  return new CreateCityController()
+interface SutTypes {
+  sut: CreateCityController
+  stateValidatorStub: StateValidator
+}
+
+const makeSut = (): SutTypes => {
+  class StateValidatorStub implements StateValidator {
+    isValid (state: string): boolean {
+      return true
+    }
+  }
+  const stateValidatorStub = new StateValidatorStub()
+  const sut = new CreateCityController(stateValidatorStub)
+  return {
+    sut,
+    stateValidatorStub
+  }
 }
 
 describe('Create City Controller', () => {
   test('Should return 400 if no name is provided ', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         state: 'valid_state'
@@ -18,7 +35,7 @@ describe('Create City Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
   test('Should return 400 if no state is provided ', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'valid_name'
@@ -27,5 +44,18 @@ describe('Create City Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('state'))
+  })
+  test('Should return 400 if an invalid state is provided ', () => {
+    const { sut , stateValidatorStub } = makeSut()
+    jest.spyOn(stateValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        state: 'invalid_state'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('state'))
   })
 })
