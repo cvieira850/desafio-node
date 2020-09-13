765dc6cc6583e862472f59f8fc630f3e4434279b
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-13 16:00:47 -0300

    test: ensure DbLoadClientById calls LoadClientByIdRepository with correct id

---
+++ src/data/usecases/load-client-by-id/db-load-client-by-id.spec.ts
@@ -0,0 +1,42 @@
+import { ClientModel } from '../../../domain/models/client'
+import { LoadClientByIdRepository } from '../../protocols/load-client-by-id-repository'
+import { DbLoadClientById } from './db-load-client-by-id'
+
+describe('DbLoadClientById', () => {
+  interface SutTypes {
+    sut: DbLoadClientById
+    loadClientByIdRepositoryStub: LoadClientByIdRepository
+  }
+
+  const makeLoadClientByIdRepository = (): LoadClientByIdRepository => {
+    class LoadClientByIdRepositoryStub implements LoadClientByIdRepository {
+      async loadById (id: string): Promise<ClientModel> {
+        const fakeClient = {
+          id: 'valid_id',
+          name: 'valid_name',
+          lastname: 'valid_lastname',
+          genre: 'valid_genre',
+          birthdate: 'valid_birthdate',
+          age: 'valid_age',
+          city: 'valid_city'
+        }
+        return new Promise(resolve => resolve(fakeClient))
+      }
+    }
+    return new LoadClientByIdRepositoryStub()
+  }
+  const makeSut = (): SutTypes => {
+    const loadClientByIdRepositoryStub = makeLoadClientByIdRepository()
+    const sut = new DbLoadClientById(loadClientByIdRepositoryStub)
+    return {
+      sut,
+      loadClientByIdRepositoryStub
+    }
+  }
+  test('Should call LoadClientByIdRepository', async () => {
+    const { sut,loadClientByIdRepositoryStub } = makeSut()
+    const loadByIdSpy = jest.spyOn(loadClientByIdRepositoryStub, 'loadById')
+    await sut.loadById('any_id')
+    expect(loadByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+})
