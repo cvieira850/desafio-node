0513dc15473f11d75b43bd6e064eac5bf3ec843b
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-19 18:52:51 -0300

    feat: ensure ChangeClientNameController return 500 if LoadClientById throws

--- src/presentation/controllers/changenameclient/changenameclient.ts
+++ src/presentation/controllers/changenameclient/changenameclient.ts
@@ -1,14 +1,18 @@
 import { Controller, HttpRequest, HttpResponse, LoadClientById } from './changenameclient-protocols'
-import { forbidden } from '../../helpers/http-helper'
+import { forbidden, serverError } from '../../helpers/http-helper'
 import { InvalidParamError } from '../../errors'
 
 export class ChangeClientNameController implements Controller {
   constructor (private readonly loadClientById: LoadClientById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const client = await this.loadClientById.loadById(httpRequest.params.id)
-    if (!client) {
-      return forbidden(new InvalidParamError('id'))
+    try {
+      const client = await this.loadClientById.loadById(httpRequest.params.id)
+      if (!client) {
+        return forbidden(new InvalidParamError('id'))
+      }
+      return null
+    } catch (error) {
+      return serverError()
     }
-    return null
   }
 }
