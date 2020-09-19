f9318c13fddb1a754482f4d1677a8b5af49454f3
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-19 18:45:05 -0300

    feat: ensure ChangeClientNameController returns 403 if invalid id is provided

--- src/presentation/controllers/changenameclient/changenameclient.ts
+++ src/presentation/controllers/changenameclient/changenameclient.ts
@@ -1,9 +1,14 @@
 import { Controller, HttpRequest, HttpResponse, LoadClientById } from './changenameclient-protocols'
+import { forbidden } from '../../helpers/http-helper'
+import { InvalidParamError } from '../../errors'
 
 export class ChangeClientNameController implements Controller {
   constructor (private readonly loadClientById: LoadClientById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadClientById.loadById(httpRequest.params.id)
+    const client = await this.loadClientById.loadById(httpRequest.params.id)
+    if (!client) {
+      return forbidden(new InvalidParamError('id'))
+    }
     return null
   }
 }
