f352b3b666b61df6b1266abe70e4de9efa8aac40
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-20 18:40:12 -0300

    feat: ensure LoadCityByNameRepository return city on success

---
+++ src/data/usecases/load-city-by-name/db-load-city-by-name-protocols.ts
@@ -0,0 +1,3 @@
+export * from '../../../domain/usecases/load-city-by-name'
+export * from '../../../domain/models/city'
+export * from '../../protocols/load-city-by-name-repository'
---
+++ src/data/usecases/load-city-by-name/db-load-city-by-name.ts
@@ -0,0 +1,9 @@
+import { LoadCityByName,CityModel,LoadCityByNameRepository } from './db-load-city-by-name-protocols'
+
+export class DbLoadCityByName implements LoadCityByName {
+  constructor (private readonly loadCityByNameRepository: LoadCityByNameRepository) {}
+  async loadByName (name: string): Promise<CityModel> {
+    const city = await this.loadCityByNameRepository.loadByName(name)
+    return city
+  }
+}
