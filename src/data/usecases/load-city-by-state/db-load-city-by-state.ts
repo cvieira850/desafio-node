5ce3b7cd2d094c295945e81d7b68661ff1cbe51d
Caio Vieira <caiodmvieiragithub@gmail.com>
2020-09-20 20:13:21 -0300

    feat: ensure LoadCityByStateRepository returns an array of cities on success

---
+++ src/data/usecases/load-city-by-state/db-load-city-by-state.ts
@@ -0,0 +1,9 @@
+import { LoadCityByState,CityModel,LoadCityByStateRepository } from './db-load-city-by-state-protocols'
+
+export class DbLoadCityByName implements LoadCityByState {
+  constructor (private readonly loadCityByStateRepository: LoadCityByStateRepository) {}
+  async loadByState (state: string): Promise<CityModel[]> {
+    const city = await this.loadCityByStateRepository.loadByState(state)
+    return city
+  }
+}
